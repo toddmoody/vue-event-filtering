be4030b73e331559d1f57d4b4044268c145aa19a
Todd Moody <todd@moodsdigital.co.nz>
2021-04-23 13:32:45 +1200

    prod build

---
+++ js/chunk-2d22d746.60e67f3b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),u={class:"about"},a=Object(e["j"])("h1",null,"This is an about page",-1);function s(n,t){return Object(e["u"])(),Object(e["g"])("div",u,[a])}const o={};o.render=s;t["default"]=o}}]);
+//# sourceMappingURL=chunk-2d22d746.60e67f3b.js.map
